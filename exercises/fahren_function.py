cb43d30c6d791f1f24c8e4bce8338d0da9d48de1
martin-martin <breuss.martin@gmail.com>
2018-07-02 20:40:41 +0200

    add function exercise

---
+++ exercises/fahren_function.py
@@ -0,0 +1,13 @@
+'''
+Celsius to Fahrenheit:
+
+Write a function to read a degree in Celsius from the console
+then convert it to Fahrenheit and print it to the console.
+
+    F = C * 1.8 + 32
+
+Output should read like - "27.4 degrees celsius = 81.32 degrees fahrenheit"
+
+NOTE: if you get an error, look up what input() returns!
+
+'''
