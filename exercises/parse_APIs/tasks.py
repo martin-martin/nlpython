7f95e6204e02d6620789a95f6ce5bd55bb539fe7
martin-martin <breuss.martin@gmail.com>
2018-07-20 21:35:02 +0200

    add JSON parsing exercises

---
+++ exercises/parse_APIs/tasks.py
@@ -0,0 +1,15 @@
+'''
+Read the different JSON files into a python script.
+You should write code that returns:
+
+* twitter.json
+    - the profile background color
+    - 'Berlin, Germany'
+    - the value of 'text' nested inside of the 'status' dict
+
+* instagram.json
+    - get username and full_name of each user in the file
+    - what tags exist in this sample file?
+    - how many of the given profiles have one or more tags?
+
+'''
