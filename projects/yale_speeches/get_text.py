b429a81571b7374a5b34f5234e748055d8986e69
martin-martin <breuss.martin@gmail.com>
2018-07-24 18:54:14 +0200

    add comments

--- projects/yale_speeches/get_text.py
+++ projects/yale_speeches/get_text.py
@@ -14,13 +14,20 @@ for f in files:
     with open(f"./pages/{f}", 'r') as fin:
         soup = BeautifulSoup(fin)
 
+    # targeting the text part of the HTML document
+    # this gives two items - one containing the date, and one the text
     div_list = soup.find_all('div', {'class': ['list-item', 'even']})
+    # getting the date as a string
     time = div_list[0].text
+    # getting the text as a list of p elements
     text_list = div_list[1].find_all('p')
-
+    # fetching only the text from those p elements
     raw_text_list = [p.text for p in text_list]
+    # creating a coherent string of all paragraphs, preserving the
+    # paragraphs as newline characters
     text = '\n'.join(raw_text_list)
 
+    # and then, writing it all to a new txt file
     with open(f'texts/{f[:-4]}.txt', 'w') as fout:
         fout.write(f'{time}\n\n')
         fout.write(text)
